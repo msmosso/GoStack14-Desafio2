963ba89598e9f02f53c34269b4ca99d5550e24c5
Matheus Mósso <msmosso@gmail.com>
2020-11-28 07:15:37 -0300

    Desafio resolvido

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
-// const { v4: uuid, validate: isUuid } = require('uuid');
+const { v4: uuid, validate: isUuid } = require('uuid');
 
 const app = express();
 
@@ -11,23 +10,51 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories)
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body
+  const repository = {
+    id: uuid(),
+    title: title,
+    url: url,
+    techs: techs,
+    likes: 0
+  }
+  repositories.push(repository)
+  return response.json(repository)
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body
+  const { id } = request.params
+  const repository = repositories.find((repo) => repo.id == id)
+  if (!repository) response.status(400)
+  repository.title = title
+  repository.url = url
+  repository.techs = techs
+  return response.json(repository)
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params
+  const repositoryIndex = repositories.findIndex(repo => repo.id === id)
+  if (repositoryIndex < 0) {
+    return response.status(400).json({ message: 'Repositório não existe!' })
+  }
+  
+  repositories.splice(repositoryIndex, 1)
+  
+  return response.status(204).send()
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params
+  const repository = repositories.find(repo => repo.id == id)
+  if (!repository) return response.status(400).json({ message: 'Repositório não existe!' })
+  repository.likes += 1
+  return response.json(repository)
 });
 
 module.exports = app;
